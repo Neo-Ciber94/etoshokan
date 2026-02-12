5ddc115e74fdffb2b12ac9d5de23c6fcf23b92c4
Freddy L <neo-ciber_94@hotmail.com>
2026-02-11 21:14:10 -0400

    Remove server code for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,31 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
+import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: getAdapter()
 	}
 };
+
+function getAdapter() {
+	if (process.env.STATIC_BUILD) {
+		return staticAdapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: "index.html",
+			precompress: false,
+			strict: true
+		});
+	}
+
+	cloudflareAdapter({
+		fallback: 'plaintext',
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		}
+	});
+}
 
 export default config;
