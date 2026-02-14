2891875b86e7ada5f6a2e261158cfe82f817f97d
Freddy L <neo-ciber_94@hotmail.com>
2026-02-14 06:33:03 -0400

    define process

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const defineProcessEnv = () => {
+	const definedEnvs = Object.fromEntries(
+		Object.entries(process.env || {}).map(([key, value]) => [
+			`process.env.${key}`,
+			JSON.stringify(value)
+		])
+	);
+
+	return definedEnvs;
+};
 
 export default defineConfig({
 	server: {
 		host: '0.0.0.0'
+	},
+	define: {
+		...defineProcessEnv(),
+		__DATE__: `'${new Date().toISOString()}'`,
+		__RELOAD_SW__: false,
+		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
 	},
 	plugins: [
 		tailwindcss(),
