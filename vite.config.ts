db4afa098775c2d7d5ff9d46b232dafc8c0358fb
Freddy L <neo-ciber_94@hotmail.com>
2026-02-14 19:40:58 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
-import dotenv from 'dotenv';
-dotenv.config();
 
-export default defineConfig(({ mode }) => {
-	const isDev = mode === 'development';
-	const processEnvs = isDev ? {} : defineProcessEnv();
-
+export default defineConfig(() => {
 	return {
 		server: {
 			host: '0.0.0.0'
 		},
 		define: {
-			//...processEnvs,
 			__DATE__: `'${new Date().toISOString()}'`,
 			__RELOAD_SW__: false,
-			'process.env.NODE_ENV':
-				process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
 		},
 		plugins: [
 			tailwindcss(),
@@ -38,14 +30,3 @@ export default defineConfig(({ mode }) => {
 		]
 	};
 });
-
-export function defineProcessEnv() {
-	const definedEnvs = Object.fromEntries(
-		Object.entries(process.env || {}).map(([key, value]) => [
-			`process.env.${key}`,
-			JSON.stringify(value)
-		])
-	);
-
-	return definedEnvs;
-}
