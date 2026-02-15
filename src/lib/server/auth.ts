37c0c6a617f114c1f2c7a3d531e9682448f9191e
Freddy L <neo-ciber_94@hotmail.com>
2026-02-15 15:32:53 -0400

    update session duration

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,13 +3,21 @@ import { betterAuth } from 'better-auth';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
+import { SESSION_DURATION_SECONDS } from '$lib/constants';
 // import { db } from '$lib/server/db';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
 	secret: env.BETTER_AUTH_SECRET,
 	//database: drizzleAdapter(db, { provider: 'sqlite' }),
-	emailAndPassword: { enabled: true },
+	//emailAndPassword: { enabled: true },
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: SESSION_DURATION_SECONDS,
+			version: '1' // change this to invalidate sessions
+		}
+	},
 	plugins: [sveltekitCookies(getRequestEvent)],
 	socialProviders: {
 		google: {
--- src/routes/api/token/+server.ts
+++ src/routes/api/token/+server.ts
@@ -1,12 +1,20 @@
 import { auth } from '$lib/server/auth';
+import type { RequestHandler } from '@sveltejs/kit';
 
-export async function GET() {
+export const GET: RequestHandler = async ({ request }) => {
+	const session = await auth.api.getSession({
+		headers: request.headers
+	});
+
+	console.log({ session });
+
 	const result = await auth.api.getAccessToken({
 		body: {
-			providerId: 'google'
+			providerId: 'google',
+			userId: session?.user.id
 		}
 	});
 
 	console.log(result);
 	return new Response();
-}
+};
