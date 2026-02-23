d91eaf70601cc9ac836b3f26ac8831923a7248ba
Freddy L <neo-ciber_94@hotmail.com>
2026-02-22 22:10:17 -0400

    prevent chrome devtools message

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,11 @@ import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 
-export async function handle(args) {
-	const { event, resolve } = args;
+export async function handle({ event, resolve }) {
+	// https://svelte.dev/docs/cli/devtools-json
+	if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
+		return new Response(undefined, { status: 404 });
+	}
+
 	return svelteKitHandler({ event, resolve, auth, building });
 }
