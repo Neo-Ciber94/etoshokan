10e63fc6a3d192dee77e6f2773b79e898a441ae5
Freddy L <neo-ciber_94@hotmail.com>
2026-02-28 15:50:44 -0400

    Convert handle deeplinking to component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import { isTauri } from '$lib/utils/isWeb';
-	import { handleAuthDeepLinking } from './handleAuthDeepLinking';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import HandleAuthDeepLinking from './HandleAuthDeepLinking.svelte';
 
 	let { children } = $props();
-
-	$effect(() => {
-		if (!isTauri()) {
-			return;
-		}
-
-		let unlisten: (() => void) | undefined;
-
-		async function run() {
-			unlisten = await handleAuthDeepLinking();
-		}
-
-		run();
-		return () => {
-			unlisten?.();
-		};
-	});
 
 	const queryClient = new QueryClient();
 </script>
 
 <QueryClientProvider client={queryClient}>
+	<HandleAuthDeepLinking />
 	{@render children()}
 </QueryClientProvider>
